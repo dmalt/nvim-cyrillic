402d5fdfb48b094a381165df10be2a3af46971cf
Dmitrii Altukhov <dm.altukhov@ya.ru>
2021-05-18 10:26:40 +0300

    Add test, refactor, fix bug
    - Add test for last input ru->en
    - Modify nvim fixture, so it doesnt fail when socket isn't
    created/removed on time
    - Refactor code asserting the cursor position after translation

--- rplugin/python3/test_plugin.py
+++ rplugin/python3/test_plugin.py
@@ -1,8 +1,8 @@
-# launch nvim with
-# NVIM_LISTEN_ADDRESS=/tmp/nvim nvim
 import os
+import os.path as op
 import subprocess
 from time import sleep
+from tempfile import mkdtemp
 
 import pytest
 from pynvim import attach
@@ -12,18 +12,27 @@ import nvim_cyrillic as plug
 
 @pytest.fixture
 def nvim():
-    os.environ["NVIM_LISTEN_ADDRESS"] = "/tmp/nvim"
+
+    tempdir = mkdtemp()
+    socket = op.join(tempdir, "nvim")
+    os.environ["NVIM_LISTEN_ADDRESS"] = socket
     p = subprocess.Popen(
         ["nvim", "--headless", "--clean"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
-    sleep(1)
+    for _ in range(50):
+        if op.exists(socket):
+            break
+        sleep(0.1)
+    else:
+        raise FileNotFoundError
     nvim = attach("socket", path=os.environ["NVIM_LISTEN_ADDRESS"])
     yield nvim
     nvim.feedkeys(nvim.replace_termcodes("<ESC>:q!<CR>"))
     p.terminate()
-    os.remove("/tmp/nvim")
+    if op.exists(socket):
+        os.remove(socket)
 
 
 def test_map_last_input_single_word_en_ru(nvim):
@@ -32,8 +41,23 @@ def test_map_last_input_single_word_en_ru(nvim):
     nvim.feedkeys(f"i{string}")
     cursor = nvim.current.window.cursor
     assert cursor[0] == 1
-    assert cursor[1] == len(string)
+    assert cursor[1] == len(string.encode("utf-8"))
     main = plug.Main(nvim)
+    main.map_last_input(args=None)
+    buf = nvim.current.buffer
+    assert len(buf) == 1
+    assert buf[0] == translation
+
+
+def test_map_last_input_single_word_ru_en(nvim):
+    string = "руддщ"
+    translation = "hello"
+    main = plug.Main(nvim)
+    main._toggle_language()
+    nvim.feedkeys(f"i{string}")
+    cursor = nvim.current.window.cursor
+    assert cursor[0] == 1
+    assert cursor[1] == len(string.encode("utf-8"))
     main.map_last_input(args=None)
     buf = nvim.current.buffer
     assert len(buf) == 1
