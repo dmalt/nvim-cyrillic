4a71cfc36e385b2d0f4b093697cc365c3a8a417a
Dmitrii Altukhov <dm.altukhov@ya.ru>
2021-05-27 11:11:05 +0300

    Make neovim load plugin for tests
    - Change neovim instance creation so it loads the plugin
    - Create remote plugins manifest (UpdateRemotePlugins) before testing
    starts
    - Call actual neovim plugin functions instead of imported python
    functions

--- rplugin/tests/test_plugin.py
+++ rplugin/tests/test_plugin.py
@@ -1,7 +1,8 @@
 import os
 import os.path as op
 import subprocess
-from tempfile import mkdtemp
+from pathlib import Path
+from tempfile import mkdtemp, mkstemp
 from time import sleep
 
 import pytest
@@ -11,6 +12,41 @@ import python3.nvim_cyrillic as plug
 
 EN = 0
 RU = 1
+PACK_DIR = Path(__file__).parent.parent.parent.parent.parent.parent.parent
+
+
+def update_manifest():
+    tempdir = mkdtemp()
+    socket = op.join(tempdir, "nvim")
+    os.environ["NVIM_LISTEN_ADDRESS"] = socket
+    temp_rplugin = mkstemp(suffix=".vim", prefix="rplugin", text=True)
+    os.environ["NVIM_RPLUGIN_MANIFEST"] = temp_rplugin[1]
+    subprocess.Popen(
+        [
+            "nvim",
+            "--headless",
+            "-u",
+            "NORC",
+            "--cmd",
+            f"set packpath+={PACK_DIR}",
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    for _ in range(50):
+        if op.exists(socket):
+            break
+        sleep(0.1)
+    else:
+        raise FileNotFoundError
+    nvim = attach("socket", path=os.environ["NVIM_LISTEN_ADDRESS"])
+    nvim.command("UpdateRemotePlugins")
+    nvim.quit()
+    if op.exists(socket):
+        os.remove(socket)
+
+
+update_manifest()
 
 
 @pytest.fixture
@@ -19,7 +55,14 @@ def nvim():
     socket = op.join(tempdir, "nvim")
     os.environ["NVIM_LISTEN_ADDRESS"] = socket
     p = subprocess.Popen(
-        ["nvim", "--headless", "--clean"],
+        [
+            "nvim",
+            "--headless",
+            "-u",
+            "NORC",
+            "--cmd",
+            f"set packpath+={PACK_DIR}",
+        ],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
@@ -33,7 +76,7 @@ def nvim():
     nvim.command("set keymap=russian-jcukenwin")
     nvim.command("set iminsert=0")
     yield nvim
-    nvim.feedkeys(nvim.replace_termcodes("<ESC>:q!<CR>"))
+    nvim.quit()
     p.terminate()
     if op.exists(socket):
         os.remove(socket)
@@ -55,13 +98,15 @@ def nvim():
 def test_map_last_input(nvim, string, transl, lang):
     # check if current language is english
     nvim.command(f"set iminsert={lang}")
-    nvim.feedkeys(f"i{string}")
+    nvim.feedkeys("i")
+    for c in string:
+        nvim.feedkeys(c)
 
     assert nvim.current.window.cursor[0] == 1
     assert nvim.current.window.cursor[1] == len(string.encode("utf-8"))
 
-    main = plug.Main(nvim)
-    main.map_last_input(args=None)
+    # nvim.feedkeys(nvim.replace_termcodes(r"<c-\><c-o>"), "t")
+    nvim.command("call MapLastInput()")
 
     assert len(nvim.current.buffer) == 1
     assert nvim.current.buffer[0] == transl
@@ -83,7 +128,9 @@ def test_map_last_input(nvim, string, transl, lang):
 def test_map_visual_word_ru_en(nvim, string, transl, lang):
     main = plug.Main(nvim)
     nvim.command(f"set iminsert={lang}")
-    nvim.feedkeys(f"i{string}")
+    nvim.feedkeys("i")
+    for c in string:
+        nvim.feedkeys(c, "t")
     nvim.feedkeys(nvim.replace_termcodes("<esc>"))
     cursor = nvim.current.window.cursor
     assert cursor[0] == 1
@@ -128,19 +175,37 @@ def test_map_visual_middle_of_word(nvim, string, transl, lang, move, targ):
         ("Научный текст \\куа", "Научный текст \\ref", RU, "", 18),
         ("Мама мыла раму \\куа", "Мама мыла раму \\reа", RU, "<Left>", 18),
         ("Quick brown pfqxbr", "Quick brown зайчик", EN, "", 18),
-        ("Quick cbybq bunny", "Quick синий bunny", EN, "<C-Left><Left>", 11,),
-        ("Something", "Something", EN, "<esc>0lla", 3,),
-        ("Smth", "elseSmth", EN, "<esc>Ielse<Right><Right>", 6,),
+        (
+            "Quick cbybq bunny",
+            "Quick синий bunny",
+            EN,
+            "<C-Left><Left>",
+            11,
+        ),
+        (
+            "Something",
+            "Something",
+            EN,
+            "<esc>0lla",
+            3,
+        ),
+        (
+            "Smth",
+            "elseSmth",
+            EN,
+            "<esc>Ielse<Right><Right>",
+            6,
+        ),
     ],
 )
 def test_map_last_input_word(nvim, string, transl, lang, move, targ):
-    main = plug.Main(nvim)
     nvim.command(f"set iminsert={lang}")
-    nvim.feedkeys(f"i{string}", options="t")
+    nvim.feedkeys("i")
+    for c in string:
+        nvim.feedkeys(c)
     nvim.feedkeys(nvim.replace_termcodes(move), options="t")
 
-    nvim.feedkeys(nvim.replace_termcodes("<c-\\><c-o>"))
-    main.map_last_input_word(args=None)
+    nvim.command("call MapLastInputWord()")
     buf = nvim.current.buffer
     assert len(buf) == 1
     assert buf[0] == transl
@@ -153,15 +218,15 @@ def test_map_last_input_word(nvim, string, transl, lang, move, targ):
     "string,transl, lang", [("Something", "Ыщьуерштп", EN)]
 )
 def test_map_last_input_word_triggered_twice(nvim, string, transl, lang):
-    main = plug.Main(nvim)
     nvim.command(f"set iminsert={lang}")
-    nvim.feedkeys(f"i{string}", options="t")
-    nvim.feedkeys(nvim.replace_termcodes("<c-\\><c-o>"))
+    nvim.feedkeys("i")
+    for c in string:
+        nvim.feedkeys(c)
 
-    main.map_last_input_word(args=None)
+    nvim.command("call MapLastInputWord()")
     assert len(nvim.current.buffer) == 1
     assert nvim.current.buffer[0] == transl
-    main.map_last_input_word(args=None)
+    nvim.command("call MapLastInputWord()")
 
     assert len(nvim.current.buffer) == 1
     assert nvim.current.buffer[0] == string
